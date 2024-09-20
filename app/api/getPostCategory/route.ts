8d1eb3334d48a216839b5e146f19ccb1f87ca3d1
kaleckh <kaleckh@gmail.com>
2024-09-20 15:52:22 -0600

    route for following

---
+++ app/api/getPostCategory/route.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+import { NextResponse, NextRequest } from 'next/server'
+
+const prisma = new PrismaClient()
+
+export async function GET(req: NextRequest) {
+    const category = req.nextUrl.searchParams.get('category');
+    try {
+        const posts = await prisma.post.findMany({
+            where: {
+                categories: {
+                    has: category
+                }
+            }
+        });
+
+        console.log(posts, 'this is post info with comments');
+        return NextResponse.json({ posts });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: 'An error occurred while fetching posts' }, { status: 500 });
+    }
+}
