2b7fd1727c2f4eea1073106d0109745bbda92169
kaleckh <kaleckh@gmail.com>
2024-09-30 12:40:44 -0600

    adding cors configurations

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -3,16 +3,31 @@ import { NextResponse, NextRequest } from 'next/server'
 
 const prisma = new PrismaClient()
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    
+    const origin = req.headers.get('origin');
+
+    // Add CORS headers to allow specific domains or all domains
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', origin || '*'); // Allow all domains or restrict to specific origin
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific methods
+    headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        // Handle preflight requests
+        return new Response(null, {
+            headers,
+            status: 204,
+        });
+    }
     console.log("hitting endpoint")
     try {
-        // Fetch all posts along with their related comments
         const posts = await prisma.post.findMany({
             include: {
-                comments: true, // Assumes there is a `comments` relation in your Prisma schema
+                comments: true,
             },
         });
-        
+
         console.log(posts, 'this is post info with comments');
         return NextResponse.json({ Posts: posts });
     } catch (error) {
